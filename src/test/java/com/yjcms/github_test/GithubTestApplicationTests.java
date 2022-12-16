7a5bf834c8b3b8a79e61c1220f4e31230ad24e91
yjcms <sen9563@gmail.com>
2022-12-16 15:24:24 +0900

    initial

---
+++ src/test/java/com/yjcms/github_test/GithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjcms.github_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
